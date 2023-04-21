c9286d60b83a728e4bffaf7ba193a8d06daab889
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-21 19:37:24 +0000

    set alert statement to test the eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByTagName("button")
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
       button.addEventListener("click", function(){
-        
+        if (this.getAttribute("id") === "rockBtn"){alert("You choose rock.")} else
+        if (this.getAttribute("id") === "paperBtn"){alert("You choose paper.")} else
+        if (this.getAttribute("id") === "scissorBtn"){alert("You choose scissor.")}  
       })
     } 
 
