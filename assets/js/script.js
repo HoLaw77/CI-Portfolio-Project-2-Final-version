bcc342c8cabf90ad70b08fbaa96d7b87bfeb3428
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-04-22 09:06:27 +0000

    remove calling gameresult function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,67 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByTagName("button");
+let playerChoice;
+let robortText = document.getElementById("robortText");
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons) {
-      button.addEventListener("click", function(){
-        if (this.getAttribute("id") === "rockBtn"){playerChoice = "rock"}
-        if (this.getAttribute("id") === "paperBtn"){playerChoice = "paper"} 
-        if (this.getAttribute("id") === "scissorsBtn"){playerChoice = "scissor"}
-        robotChoice();
-        gameResult();
-        if (resultText.textcontent === "Congratulation! You win!"){incrementWin()};
-        if (resultText.textcontent === "Sorry, you lose."){incrementLose()};
-        if (resultText.textcontent === "Draw"){incrementDraw()};
-      })
-    } 
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("id") === "rockBtn") {
+        playerChoice = "rock"
+      }
+      if (this.getAttribute("id") === "paperBtn") {
+        playerChoice = "paper"
+      }
+      if (this.getAttribute("id") === "scissorsBtn") {
+        playerChoice = "scissor"
+      }
+      robotChoice();
+      gameResult();
+      if (resultText.textcontent === "Congratulation! You win!") {
+        incrementWin()
+      };
+      if (resultText.textcontent === "Sorry, you lose.") {
+        incrementLose()
+      };
+      if (resultText.textcontent === "Draw") {
+        incrementDraw()
+      };
+    })
+  }
+})
+
+
+function robotChoice() {
+  const numAsChoice = Math.floor(Math.random() * 3) + 1
+  robortText = document.getElementById("robortChoice");
+
+  if (numAsChoice == 1) {
+    robortText.innerHTML = "rock"
+  } else
+  if (numAsChoice == 2) {
+    robortText.innerHTML = "paper"
+  } else
+  if (numAsChoice == 3) {
+    robortText.innerHTML = "scissor"
+  }
+  
 }
-) 
 
-function robotChoice(){
-  const numAsChoice = Math.floor(Math.random() * 3)+1
-
-  if (numAsChoice == 1){robortChoice = "rock"} else 
-  if (numAsChoice == 2){robortChoice = "paper"} else
-  if (numAsChoice == 3){robortChoice = "scissor"}
-  robortChoice = document.getElementById("robortText");
-  gameResult();
-}
-function gameResult(){
-  resultText = document.getElementById("resultText")
-  if (playerChoice === robortChoice) {resultText = "Draw"} else 
-  if (playerChoice === "rock" && robortChoice === "paper"){resultText = "Sorry, you lose."} else 
-  if (playerChoice === "paper" && robortChoice === "scissor"){resultText = "Sorry, you lose."}else
-  if (playerChoice === "scissor" && robortChoice === "rock"){resultText = "Sorry, you lose."}else 
-  {resultText = "Congratulation! You win!" }
+function gameResult() {
+  resultText.innerHTML = document.getElementById("resultText")
+  if (playerChoice === robortChoice) {
+    resultText.innerHTML = "Draw"
+  } else
+  if (playerChoice === "rock" && robortChoice === "paper") {
+    resultText.innerHTML = "Sorry, you lose."
+  } else
+  if (playerChoice === "paper" && robortChoice === "scissor") {
+    resultText.innerHTML = "Sorry, you lose."
+  } else
+  if (playerChoice === "scissor" && robortChoice === "rock") {
+    resultText.innerHTML = "Sorry, you lose."
+  } else {
+    resultText.innerHTML = "Congratulation! You win!"
+  }
 
 }
 
@@ -39,19 +69,18 @@ function incrementWin() {
 
   let score = parseInt(document.getElementById("win").innerText);
   document.getElementById("win").innerText = ++score;
-  
-  }
-  
-  function incrementDraw() {
-  
+
+}
+
+function incrementDraw() {
+
   let score = parseInt(document.getElementById("draw").innerText);
   document.getElementById("draw").innerText = ++score;
-  
-  }
-  
-  function incrementLose() {
-  
-  let score = parseInt(document.getElementById("lose").innerText);
-  document.getElementById("lose").innerText = ++score;}
-  
 
+}
+
+function incrementLose() {
+
+  let score = parseInt(document.getElementById("lose").innerText);
+  document.getElementById("lose").innerText = ++score;
+}
